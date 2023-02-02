0332acad108f8f776286d83cbcf37d54b74fc7f0
Ivan Chikish <nekotekina@gmail.com>
2023-02-02 19:19:58 +0300

    Remove <Windows.h> dependency

--- csharp_types.h
+++ csharp_types.h
@@ -3,7 +3,11 @@
 #include<fstream>
 #include<vector>
 #include<map>
+#ifdef _WIN32
 #include<Windows.h>
+#else
+typedef char BYTE;
+#endif
 
 typedef unsigned int uint;
 typedef unsigned long ulong;
